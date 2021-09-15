fc2c5998dcabf62515bef35642c87447e700b8b4
Rohan <rohanzealous@gmail.com>
2021-09-15 13:46:37 -0700

    finished

--- src/main/java/MyMain.java
+++ src/main/java/MyMain.java
@@ -1,11 +1,12 @@
+import static java.lang.Character.toLowerCase;
+
 public class MyMain {
 
     // Changes a capital letter to a lowercase letter
     // If the char is not an uppercase letter, 
     // it is returned unchanged. 
     public static char toLower(char ch) {
-        // REPLACE WITH YOUR CODE
-        return '!';
+        return Character.toLowerCase(ch);
     }
 
     // Changes a lowercase letter to an uppercase letter
@@ -13,19 +14,18 @@ public class MyMain {
     // it is returned unchanged.
     public static char toUpper(char ch) {
         // REPLACE WITH YOUR CODE HERE
-        return '!';
+        return Character.toUpperCase(ch);
     }
 
     // The method is given a String as input and returns a
     // new String where all of the numbers have been removed
     // from the original String.
     public static String removeNumbers(String str) {
-        // REPLACE WITH YOUR CODE
-        return "!!!";
+        return str.replaceAll("[1-9]","");
     }
 
     // Write some code to test your methods!
     public static void main(String[] args) {
-        // ADD SOME TESTING CODE HERE
+
     }
 }
